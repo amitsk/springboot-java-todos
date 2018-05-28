90606481deaf26dff409d58f070b77cc0d19506c
amit <amitsk1974@gmail.com>
2018-05-28 10:16:37 -0700

    Switched to Undertow

--- src/main/java/com/github/amitsk/sunrise/SpringBootSunriseApplication.java
+++ src/main/java/com/github/amitsk/sunrise/SpringBootSunriseApplication.java
@@ -1,12 +1,15 @@
 package com.github.amitsk.sunrise;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSunriseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSunriseApplication.class, args);
+        SpringApplication app = new SpringApplication(SpringBootSunriseApplication.class);
+        app.setWebApplicationType(WebApplicationType.REACTIVE);
+        app.run(args);
     }
 }
--- src/main/java/com/github/amitsk/sunrise/handlers/SunriseExceptionHandler.java
+++ src/main/java/com/github/amitsk/sunrise/handlers/SunriseExceptionHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.codec.HttpMessageWriter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.reactive.function.server.HandlerStrategies;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.reactive.result.view.ViewResolver;
