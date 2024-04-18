cd0d59492ff279e7a6c59f89c8bf4c10d56b67e6
Amit K <amitsk1974@gmail.com>
2024-04-17 20:57:23 -0700

    Tests fixed

--- src/test/java/com/github/amitsk/sunrise/service/SunriseApiClientTest.java
+++ src/test/java/com/github/amitsk/sunrise/service/SunriseApiClientTest.java
@@ -7,7 +7,6 @@ import com.github.amitsk.sunrise.model.SunsetApiResponse;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -50,20 +49,20 @@ public class SunriseApiClientTest {
 
             // Ask the server for its URL. You'll need this to make HTTP requests.
             baseUrl = server.url("/json/");
+            SunriseProperties sunriseProperties = new SunriseProperties();
+            SunriseProperties.SunriseApi sunriseApi = new SunriseProperties.SunriseApi();
+            sunriseApi.setUrl(baseUrl.toString());
+            sunriseProperties.setSunriseApi(sunriseApi);
+
+            WebClient webClient = WebClient.create(baseUrl.toString());
+            SunriseApiClient sunriseApiClient = new SunriseApiClient(webClient);
+
+            SunsetApiResponse.SunsetSunrise sunsetSunrise = sunriseApiClient.callApi(new SunriseRequest("2.00", "2.00")).block();
+            Assertions.assertNotNull(sunsetSunrise);
+            assertThat(sunsetSunrise.sunrise()).isEqualTo("A");
+            assertThat(sunsetSunrise.sunset()).isEqualTo("B");
         }
 
-        SunriseProperties sunriseProperties = new SunriseProperties();
-        SunriseProperties.SunriseApi sunriseApi = new SunriseProperties.SunriseApi();
-        sunriseApi.setUrl(baseUrl.toString());
-        sunriseProperties.setSunriseApi(sunriseApi);
-
-        WebClient webClient = WebClient.create(baseUrl.toString());
-        SunriseApiClient sunriseApiClient = new SunriseApiClient(webClient);
-
-        SunsetApiResponse.SunsetSunrise sunsetSunrise = sunriseApiClient.callApi(new SunriseRequest("2.00", "2.00")).block();
-        Assertions.assertNotNull(sunsetSunrise);
-        assertThat(sunsetSunrise.sunrise()).isEqualTo("A");
-        assertThat(sunsetSunrise.sunset()).isEqualTo("B");
 
     }
 
