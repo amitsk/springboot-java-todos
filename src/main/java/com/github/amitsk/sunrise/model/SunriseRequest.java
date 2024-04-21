879d0a41440ff568a978786be1b4d87e10d24b99
Amit K <amitsk1974@gmail.com>
2024-04-21 13:56:21 -0700

    Actuator changes

--- src/main/java/com/github/amitsk/sunrise/model/SunriseRequest.java
+++ src/main/java/com/github/amitsk/sunrise/model/SunriseRequest.java
@@ -1,10 +1,15 @@
 package com.github.amitsk.sunrise.model;
 
 
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 
 public record SunriseRequest(
-        @DecimalMin("-90.00") String lat,
-        @DecimalMin("-180.00") String lng) {
+        @DecimalMin("-90.00")
+        @DecimalMax("90.00")
+        String lat,
+        @DecimalMin("-180.00")
+        @DecimalMax("180.00")
+        String lng) {
 }
 
